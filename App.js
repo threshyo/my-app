3b1f5498fec87aa731570720855569c3e83ea123
Andrew <andrewthresh@yahoo.co.uk>
2021-11-19 09:57:28 -0500

    hi

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import download from "./assets/Dog.jpeg";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+  let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{
+          uri: "https://www.thesprucepets.com/thmb/irfGtaNrycY9wO2MiScSQDzd-TE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/top-friendliest-dog-breeds-4691511_hero-5c6a918dcf56409c888d78b0fac82d18.jpg",
+        }}
+        style={{ width: 305, height: 150 }}
+      />
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={{ backgroundColor: "blue" }}
+      >
+        <Text style={{ fontSize: 20, color: "#fff" }}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +67,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#3ea0ad",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  },
+  buttonText: { fontSize: 20, color: "#fff" },
 });
